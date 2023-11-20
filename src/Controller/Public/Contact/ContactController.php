02d7bd28d71f9fbf7eb592aea3f3326121684576
Novitz Jean-Philippe <novitz@gmail.com>
2023-11-21 00:13:14 +0100

    Update: contactController
    Externalize send message action to sendMessage service

--- src/Controller/Public/Contact/ContactController.php
+++ src/Controller/Public/Contact/ContactController.php
@@ -2,26 +2,37 @@
 
 namespace App\Controller\Public\Contact;
 
+use App\Contract\SendMessageInterface;
 use App\Entity\Message;
 use App\Form\MessageType;
 use App\Repository\CompanyRepository;
 use App\Repository\MessageRepository;
+use App\Service\SendMessage;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
+    /**
+     * @throws TransportExceptionInterface
+     */
     #[Route('/contact', name: 'app_contact')]
-    public function index(CompanyRepository $companyRepository, EntityManagerInterface $entityManager, Request $request): Response
+    public function index(CompanyRepository $companyRepository,
+                          EntityManagerInterface $entityManager,
+                          SendMessageInterface $sendMessage,
+                          Request $request): Response
     {
 
         $form = $this->createForm(MessageType::class);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $datas = $form->getData();
             $message = new Message();
             $message->setName($datas->getName());
@@ -30,7 +41,9 @@ class ContactController extends AbstractController
             $message->setMessage($datas->getMessage());
             $entityManager->persist($message);
             $entityManager->flush();
-                $this->addFlash('success', 'Votre message a été envoyé.');
+            $this->addFlash('success', 'Votre message a été envoyé.');
+
+           $sendMessage($datas->getEmail(),$datas->getPhone(), $datas->getMessage());
             return $this->redirectToRoute('app_contact');
 
         }
