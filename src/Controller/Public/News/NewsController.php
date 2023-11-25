abbf87fd25881e851cf74ebdd287b5f65d4c9ec6
Novitz Jean-Philippe <novitz@gmail.com>
2023-11-26 00:12:56 +0100

    Add: new Controller action
    Add show action
    
    - controller action
    - show template

--- src/Controller/Public/News/NewsController.php
+++ src/Controller/Public/News/NewsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Public\News;
 
+use App\Entity\News;
 use App\Repository\NewsRepository;
 use Knp\Component\Pager\Pagination\PaginationInterface;
 use Knp\Component\Pager\PaginatorInterface;
@@ -12,15 +13,22 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class NewsController extends AbstractController
 {
-    #[Route('/news/', name: 'app_news')]
+    #[Route('/articles/', name: 'app_news')]
     public function index(NewsRepository $newsRepository, PaginatorInterface $paginator, Request $request ): Response
     {
-        // dd($newsRepository->findAlStartingAt()->getResult());
         $news = $paginator->paginate(
             $newsRepository->findAlStartingAt(),
             $request->query->getInt('page', 1),
             $this->getParameter('app.news_per_page'));
         return $this->render('themes/' . $this->getParameter('app.theme') . '/news/index.html.twig', [
+            'news' => $news,
+        ]);
+    }
+
+    #[Route('/article/{slug}', name: 'app_news_show')]
+    public function show(News $news, Request $request ): Response
+    {
+        return $this->render('themes/' . $this->getParameter('app.theme') . '/news/show.html.twig', [
             'news' => $news,
         ]);
     }
